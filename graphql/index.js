f08e195ed51c342342f474320822e8be40fa291c
juanmoide <juanma.dev23@gmail.com>
2021-03-26 19:55:20 +0100

    Added TypeDefs example

--- graphql/index.js
+++ graphql/index.js
@@ -1,7 +1,56 @@
 import { gql } from 'apollo-server-micro';
 
 export const typeDefs = gql`
+interface AwardData {
+    id: ID!
+    name: String!
+}
+
+type Award implements AwardData {
+    id: ID!
+    name: String!
+}
+
+type AwardWon implements AwardData {
+    id: ID!
+    name: String!
+    year: Int!
+    winner: Actor!
+}
+
+interface BasicData {
+    id: ID!
+    name: String!
+    age: Int!
+}
+
+type People implements BasicData {
+    id: ID!
+    name: String!
+    age: Int!
+}
+
+type Actor implements BasicData {
+    id: ID!
+    name: String!
+    age: Int!
+    films: [Film!]!
+    awards: [AwardWon!]!
+}
+
+
+type Film {
+    id: ID!
+    name: String!
+    director: People!
+    actors: [Actor!]!
+    recommended: Boolean
+}
+
 type Query {
-  hello(name: String): String!
+  allFilms: [Film!]!
+  allActors: [Actor!]!
+  allAwards: [Award!]!
+  allPeople: [People]
 }
 `
