088afd2df606d8d95698e656755eb2bf78183829
juanmoide <juanma.dev23@gmail.com>
2021-03-26 18:49:10 +0100

    :tada: Initial commit

---
+++ api/graphql.js
@@ -0,0 +1,53 @@
+import { ApolloServer, gql } from 'apollo-server-micro';
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+}
+
+const path = '/api/graphql'
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req, connection }) => {
+    if (connection) {
+      // check connection for metadata
+      return connection.context;
+    }
+    // get some data from the request
+    return {};
+  },
+  introspection: true,
+  subscriptions: {
+    path
+  },
+  playground: {
+    subscriptionEndpoint: path,
+  },
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const graphqlWithSubscriptionHandler = (req, res, next) => {
+  if (!res.socket.server.apolloServer) {
+    apolloServer.installSubscriptionHandlers(res.socket.server);
+    const handler = apolloServer.createHandler({ path });
+    res.socket.server.apolloServer = handler;
+  }
+
+  return res.socket.server.apolloServer(req, res, next);
+};
+
+export default graphqlWithSubscriptionHandler;
